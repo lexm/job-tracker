42f64597a2fae6e37042d8cc26df841e9bb4c3e6
Lex Myers <lex.myers@gmail.com>
2017-04-06 09:18:53 -0700

    added sqlizeTest.js to sandbox

--- sandbox/sqlizeTest.js
+++ sandbox/sqlizeTest.js
@@ -10,3 +10,29 @@ var sequelize = new Sequelize(process.env.USER, process.env.USER, '', {
     idle: 10000
   }
 });
+
+var Employer = sequelize.define('employer', {
+  employerName: {
+    type: Sequelize.STRING,
+    field: 'employer_name'
+  }
+})
+
+var Position = sequelize.define('position', {
+  jobTitle: {
+    type: Sequelize.STRING,
+    field: 'job_title'
+  },
+  extPosID: {
+    type: Sequelize.STRING,
+    field: 'ext_pos_id'
+  },
+  location: {
+    type: Sequelize.STRING,
+    field: 'location'
+  },
+  appDate: {
+    type: Sequelize.database,
+    field: 'app_date'
+  },
+})
